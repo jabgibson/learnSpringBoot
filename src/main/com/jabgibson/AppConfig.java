245bbe6ed288d8ec60e899307985431f32074fd4
Brandon Gibson <jab.gib@gmail.com>
2016-04-30 13:01:57 -0700

    First real commit. Basic structure

---
+++ src/main/com/jabgibson/AppConfig.java
@@ -0,0 +1,16 @@
+package main.com.jabgibson;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by bgibson on 4/30/16.
+ */
+@Configuration
+@ComponentScan
+@EnableWebMvc
+
+public class AppConfig {
+
+}
---
+++ src/main/com/jabgibson/WebAppInitializer.java
@@ -0,0 +1,25 @@
+package main.com.jabgibson;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Created by bgibson on 4/30/16.
+ */
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.setServletContext(servletContext);
+        ServletRegistration.Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        dynamic.addMapping("/");
+        dynamic.setLoadOnStartup(1);
+    }
+}
